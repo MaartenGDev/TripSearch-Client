2d4b085e7bfdadfd60c06a92424e178281ce23b6
MaartenGDev <maarten.grootoonk@gmail.com>
2016-10-14 12:23:49 +0200

    Added suggestions

--- src/js/components/search/SearchBar.js
+++ src/js/components/search/SearchBar.js
@@ -2,26 +2,12 @@ import React from 'react';
 
 const SearchBar = (props) => {
 
-    let searchQuery = props.searchQuery;
-    const analyseResult = props.analyseResult;
-    let replacements = [];
-    Object.keys(analyseResult).forEach((key) => {
-        const item = analyseResult[key];
-
-        let search = searchQuery.substr(item.start, item.end - item.start);
-
-        replacements.push(search);
-    });
-
-    replacements.forEach((item) => {
-        return searchQuery = searchQuery.replace(item, '<span style="color: red">' + item + '</span>');
-    });
 
     return (
         <div className="planner-search">
             <i id="search-button" className="fa fa-search" aria-hidden="true" onClick={props.search}></i>
             <div contentEditable="true" className="search-input" id="search" onKeyUp={props.onChange}
-                 dangerouslySetInnerHTML={{__html: searchQuery}}/>
+                 dangerouslySetInnerHTML={{__html: props.searchQuery}}/>
         </div>
     )
 };
--- src/js/components/search/SearchPage.js
+++ src/js/components/search/SearchPage.js
@@ -10,6 +10,7 @@ class SearchPage extends React.Component {
             analyseResult: {},
             result: {
                 search: '',
+                searchHtml: '',
                 result_title: '',
                 data: {},
             }
@@ -17,11 +18,12 @@ class SearchPage extends React.Component {
 
         this.search = this.search.bind(this);
         this.handleTypeEvent = this.handleTypeEvent.bind(this);
+        this.highlightKeywords = this.highlightKeywords.bind(this);
+        this.resetTextSearch = this.resetTextSearch.bind(this);
     }
 
     search() {
         let form = new FormData();
-
         form.append('search', this.state.result.search);
 
         fetch('https://search.dev/', {
@@ -29,13 +31,19 @@ class SearchPage extends React.Component {
             body: form
         })
             .then((res) => res.json())
-            .then((data) => this.setState({result: data}));
-
+            .then((data) => this.setState({
+                result: {
+                    search: data.search,
+                    searchHtml: data.search,
+                    result_title: data.result_title,
+                    data: data.data,
+                }
+            }));
     }
 
-    getSuggestions(){
-        let form = new FormData();
+    getSuggestions(startPos, anchorOffset, target) {
 
+        let form = new FormData();
         form.append('search', this.state.result.search);
 
         fetch('https://search.dev/analyse.php', {
@@ -43,14 +51,69 @@ class SearchPage extends React.Component {
             body: form
         })
             .then((res) => res.json())
-            .then((data) => this.setState({analyseResult: data}));
+            .then((data) => {
+
+                this.resetTextSearch(target);
+
+                this.setState({
+                    analyseResult: data
+                });
+
+
+                this.setState({
+                    result: {
+                        search: this.state.result.search,
+                        searchHtml: this.stripHtml(this.state.result.searchHtml),
+                        data: this.state.result.data,
+                        result_title: this.state.result.result_title
+                    }
+                });
+
+
+            });
+
+
+    }
+
+    resetTextSearch(target) {
+
+
+
+    }
+
+    highlightKeywords(sentence) {
+        let searchHtml = this.stripHtml(sentence);
+
+        const analyseResult = this.state.analyseResult;
+
+        let replacements = [];
+
+        Object.keys(analyseResult).forEach((key) => {
+            const item = analyseResult[key];
+
+            let search = searchHtml.substr(item.start, item.end - item.start);
+
+            replacements.push(search);
+        });
+
+        replacements.forEach((item) => {
+            searchHtml = searchHtml.replace(item, '<span style="color: red">' + item + '</span>');
+        });
+        return searchHtml;
+    }
+
+    stripHtml(html) {
+        var tmp = document.createElement("DIV");
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText;
     }
 
     handleTypeEvent({target}) {
-        this.getSuggestions();
+        const search = this.stripHtml(target.innerText);
         this.setState({
             result: {
-                search: target.innerHTML,
+                search: search,
+                searchHtml: search,
                 result_title: this.state.result.result_title,
                 data: this.state.result.data,
             }
@@ -66,12 +129,13 @@ class SearchPage extends React.Component {
         return (
             <main>
                 <section className="card">
-                    <SearchBar analyseResult={this.state.analyseResult} onChange={this.handleTypeEvent} searchQuery={this.state.result.search}
+                    <SearchBar analyseResult={this.state.analyseResult} onChange={(node) => this.handleTypeEvent(node)}
+                               searchQuery={this.state.result.searchHtml}
                                search={() => this.search()}/>
                 </section>
 
                 {hasResult ? <section className="card planner-search-result-preview"
-                         style={ hasResult ? {backgroundImage: 'url(' + mainBackground + ')'} : null}>
+                                      style={ hasResult ? {backgroundImage: 'url(' + mainBackground + ')'} : null}>
                     <SearchResult title={this.state.result.result_title} result={this.state.result.data}/>
                 </section> : null}
             </main>
